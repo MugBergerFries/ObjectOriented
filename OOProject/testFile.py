ddf39bc4147b3fdafaa47fd45a94867adb0841a7
MugBergerFries <sjamesberger@gmail.com>
2019-03-13 07:51:10 -0600

    added index.html

--- OOProject/testFile.py
+++ OOProject/testFile.py
@@ -1,4 +1,9 @@
 import spotipy
+import spotipy.util as util
+
+token = util.prompt_for_user_token('MugBergerFries', 'user-library-read', client_id='0ffe4f5e083f464f8ad6061cd80785ca',
+                                   client_secret='e1c15024a0c744a792d729510575a0ca', redirect_uri='http://localhost'
+                                                                                                  ':8888/callback')
 spotify = spotipy.Spotify()
 results = spotify.search(q='artist:' + 'Johari', type='artist')
 print(results)
