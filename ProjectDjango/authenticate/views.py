a9812835d4bbb112e1cd0e59350e361e762bbcd7
SamBusser30 <sabu0818@colorado.edu>
2019-03-18 23:14:05 -0600

    Update views.py

--- ProjectDjango/authenticate/views.py
+++ ProjectDjango/authenticate/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<img src="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi7jtn9t43hAhWFoYMKHUVUAu8QjRx6BAgBEAU&url=https%3A%2F%2Ftvtropes.org%2Fpmwiki%2Fpmwiki.php%2FFunny%2FTheOfficeUS&psig=AOvVaw2XVpc43CMGkYX4efHKFMYk&ust=1553058518588365" alt="W3Schools.com">)
+    return HttpResponse('<img src="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi7jtn9t43hAhWFoYMKHUVUAu8QjRx6BAgBEAU&url=https%3A%2F%2Ftvtropes.org%2Fpmwiki%2Fpmwiki.php%2FFunny%2FTheOfficeUS&psig=AOvVaw2XVpc43CMGkYX4efHKFMYk&ust=1553058518588365" alt="W3Schools.com">
