005da231c0cd907aab63bb85de5b1e08e9a7dde1
SamBusser30 <sabu0818@colorado.edu>
2019-04-03 01:18:36 -0600

    testing spotify api

--- ProjectDjango/authenticate/views.py
+++ ProjectDjango/authenticate/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 
 
 def home(request):
+    HttpResponse(https://api.spotify.com/v1	/v1/artists/{3TVXtAsR1Inumwj472S9r4})
     return render(request, 'authenticate/home.html')
 
 
