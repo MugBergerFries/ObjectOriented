86112d2f98bbdda72069404a0d7803206298aac2
SamBusser30 <sabu0818@colorado.edu>
2019-04-03 18:05:47 -0600

    removing bugs

--- ProjectDjango/authenticate/views.py
+++ ProjectDjango/authenticate/views.py
@@ -3,11 +3,6 @@ from django.http import HttpResponse
 import spotipy
 import spotipy.util as util
 
-def show_tracks(track_list):
-    for i, item in enumerate(track_list['items']):
-        track = item['track']
-        print("   %d %32.32s %s" % (i, track['artists'][0]['name'], track['name']))
-
 def home(request):
     HttpResponse('https://api.spotify.com/v1/artists/{3TVXtAsR1Inumwj472S9r4}')
     return render(request, 'authenticate/home.html')
