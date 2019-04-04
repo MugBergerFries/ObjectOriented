ae600b7a92fc515882c4336cf204af79712e33f5
MugBergerFries <sjamesberger@gmail.com>
2019-04-04 01:15:25 -0600

    added post request for token

--- ProjectDjango/authenticate/views.py
+++ ProjectDjango/authenticate/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
-import json
-import spotipy
-import spotipy.util as util
+import requests
+import base64
+import os
 
 client_id = '0ffe4f5e083f464f8ad6061cd80785ca'
-redirect_uri = 'http://ec2-18-191-18-199.us-east-2.compute.amazonaws.com/callback/'
+redirect_uri1 = 'http://ec2-18-191-18-199.us-east-2.compute.amazonaws.com/callback/'
+redirect_uri2 = 'http://ec2-18-191-18-199.us-east-2.compute.amazonaws.com/about/'
+token = 'NULL'
 
 
 def home(request):
@@ -20,13 +22,19 @@ def about(request):
 
 def login(request):
     return redirect('https://accounts.spotify.com/authorize?client_id=' + client_id + '&response_type=code'
-                    '&redirect_uri=' + redirect_uri + '&scope=user-read-private')
+                    '&redirect_uri=' + redirect_uri1 + '&scope=user-read-private')
 
 
 def callback(request):
-    test = request.GET.get('code', '')
-    print(test)
-    return
+    code = request.GET.get('code', '')
+    if code == '':
+        print("AN ERROR OCCURRED, REDIRECTING HOME")
+        return render(request, 'authenticate/index.html')
+    encoded = base64.b64encode(client_id + ':' + os.environ['SPOTIPY_CLIENT_SECRET'])
+    payload = {'grant_type': 'authorization_code', 'code': code, 'redirect_uri': redirect_uri2}
+    r = requests.post('https://accounts.spotify.com/api/token', headers={'Authorization', 'Basic ' + encoded},
+                      data=payload)
+    return render(request, 'authenticate/about.html')
 
 
 def refresh_token(request):
