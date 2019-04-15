b39d4e108c972930b6350886c2eaa0106a6077b7
SamBusser30 <sabu0818@colorado.edu>
2019-04-15 14:45:14 -0600

    debug

--- ProjectDjango/prune/views.py
+++ ProjectDjango/prune/views.py
@@ -5,11 +5,11 @@ from django.shortcuts import redirect
 
 def choose(request):
     context = request.GET.get('context')
-    print(context)
+    print("HERE IS CONTEXT",context)
     return render(request, 'prune/choose.html')
 
 def magic(request):
     print("TEST")
     #if request.method == 'POST':
-    print("LOOK HERE", request.POST.get())
+    #print("LOOK HERE", request.POST.get())
     return render(request, 'prune/magic.html')
