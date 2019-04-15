a8490cf61818d4de58eb6d3c9b0076c250ef0dfa
SamBusser30 <sabu0818@colorado.edu>
2019-04-15 00:49:20 -0600

    Missed some server stuff

--- ProjectDjango/prune/urls.py
+++ ProjectDjango/prune/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'prune'
 
 urlpatterns = [
-    path('', views.choose, name='prune-choose')
+    path('', views.choose, name='prune-choose'),
+    path('magic/', views.magic, name='prune-magic')
+
 ]
