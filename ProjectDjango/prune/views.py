b40eea4b31f934e944e6c8cb18a5dbf7bb5369d0
SamBusser30 <sabu0818@colorado.edu>
2019-04-15 14:08:48 -0600

    testing with determining which button was clicked

--- ProjectDjango/prune/views.py
+++ ProjectDjango/prune/views.py
@@ -9,4 +9,7 @@ def choose(request):
     return render(request, 'prune/choose.html')
 
 def magic(request):
+    if request.method == 'POST':
+        form = PriceAssessmentSection1(request.POST)
+        print(request.POST.get())
     return render(request, 'prune/magic.html')
