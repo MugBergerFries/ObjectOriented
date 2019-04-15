50580c0806ce2b9a7d49b48dcbfb3bb2e31dc526
SamBusser30 <sabu0818@colorado.edu>
2019-04-15 15:11:21 -0600

    debug printing

--- ProjectDjango/prune/views.py
+++ ProjectDjango/prune/views.py
@@ -11,6 +11,8 @@ def choose(request):
             print("It worked")
         elif request.POST.get("save_next"):  # You can use else in here too if there is only 2 submit types.
             return HttpResponseRedirect(reverse('portal_sec2'))
+
+    print("IS THIS WORKING")
     return render(request, 'prune/choose.html')
 
 def magic(request):
