10f54f8e8f868b8aff7a152fad720ed75e161726
SamBusser30 <sabu0818@colorado.edu>
2019-04-15 14:16:50 -0600

    debugging

--- ProjectDjango/prune/views.py
+++ ProjectDjango/prune/views.py
@@ -9,6 +9,7 @@ def choose(request):
     return render(request, 'prune/choose.html')
 
 def magic(request):
+    print("TEST")
     if request.method == 'POST':
         form = PriceAssessmentSection1(request.POST)
         print("LOOK HERE", request.POST.get())
