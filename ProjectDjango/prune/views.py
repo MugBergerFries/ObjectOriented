89fd880ce20dc588d75f0cf4f697d6859e0bbbcc
SamBusser30 <sabu0818@colorado.edu>
2019-04-15 23:43:43 -0600

    reverting

--- ProjectDjango/prune/views.py
+++ ProjectDjango/prune/views.py
@@ -2,13 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 import requests
-import base64
-import os
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
 
-client_id = '0ffe4f5e083f464f8ad6061cd80785ca'
-redirect_uri1 = 'http://ec2-18-191-18-199.us-east-2.compute.amazonaws.com/magic/'
+
 
 def choose(request):
     # context = request.GET.get('context')
@@ -23,23 +18,6 @@ def magic(request):
     #print("LOOK HERE", request.POST.get())
     test = request.GET.get('playlist')
     #print("ID HERE", test)
-    url = 'https://accounts.spotify.com/api/token'
-    encoded = base64.b64encode("{}:{}".format(client_id, os.environ['SPOTIPY_CLIENT_SECRET']).encode('utf-8')).decode(
-        'utf-8')
-    payload = {"grant_type": "authorization_code", "code": str(code), "redirect_uri": redirect_uri1}
-    headers = {'Authorization': 'Basic ' + encoded}
-    req = requests.post(url, data=payload, headers=headers)
-    global token
-    response_list = req.json()
-    token = response_list['access_token']
-    #print("TOKEN: " + token)
-
-    client_credentials_manager = SpotifyClientCredentials(client_id=client_id,
-                                                          client_secret='e1c15024a0c744a792d729510575a0ca')
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-    # get users information
-    headers = {'Authorization': 'Bearer ' + token}
     songs = requests.get('https://api.spotify.com/v1/playlists/'+test+'/tracks')
     resp = songs.json()
     print(resp)
