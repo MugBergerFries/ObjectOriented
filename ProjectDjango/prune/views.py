a0132e0ffa1fc827c24d4552921b4ce8f47fc210
SamBusser30 <sabu0818@colorado.edu>
2019-04-22 20:25:52 -0600

    Trying to hard code delete request

--- ProjectDjango/prune/views.py
+++ ProjectDjango/prune/views.py
@@ -124,8 +124,10 @@ def remove(request):
     tracks = {"tracks":[{"uri": "spotify:track:"+song_id, "positions":[order]}]}
     tracks = str(tracks)
     data = '{"tracks":[{"uri":"spotify:track:3DNRdudZ2SstnDCVKFdXxG","positions":[0]}]}'
-    songs = requests.delete('https://api.spotify.com/v1/playlists/'+str(playlist_id)+'/tracks',  headers=headers, data=data)
-    print("RESPONSE", songs)
+    #songs = requests.delete('https://api.spotify.com/v1/playlists/'+str(playlist_id)+'/tracks',  headers=headers, data=data)
+    response = requests.delete('https://api.spotify.com/v1/playlists/26S6d4nIGuMeKkRhJ2tuAI/tracks', headers=headers, data=data)
+
+    print("RESPONSE", response)
 
     context = {'remove_song_id':song_id, 'remove_order':order, 'remove_token': token}
     return render(request, 'prune/remove.html', context)
