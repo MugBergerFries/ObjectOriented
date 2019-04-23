0dba0637e9a7314c159cf3265e28ea8fa102e01f
SamBusser30 <sabu0818@colorado.edu>
2019-04-22 19:14:01 -0600

    debuggin

--- ProjectDjango/prune/views.py
+++ ProjectDjango/prune/views.py
@@ -123,7 +123,7 @@ def remove(request):
     headers = {'Authorization': 'Bearer ' + token}
     tracks = {"tracks":[{"uri": "spotify:track:"+song_id, "positions":[order]}]}
     tracks = str(tracks)
-    songs = requests.delete('https://api.spotify.com/v1/playlists/'+playlist_id+'/tracks',  headers=headers, data=tracks,)
+    songs = requests.delete('https://api.spotify.com/v1/playlists/'+playlist_id+'/tracks',  headers=headers, data=tracks)
     print("RESPONSE", songs)
 
     context = {'remove_song_id':song_id, 'remove_order':order, 'remove_token': token}
